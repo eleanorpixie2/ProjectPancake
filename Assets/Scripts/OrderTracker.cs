e829267e983610870cf752dd52963322c955aebe
Jethro Schoppenhorst <Jethro.Schoppenhorst@iam.local>
2018-10-12 13:01:37 -0500

    timer and counters

---
+++ Assets/Scripts/OrderTracker.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrderTracker : MonoBehaviour {
+
+    // Inspector fields for player counter ui texts.
+    [SerializeField]
+    private Text[] playerSuccessCounters;
+    [SerializeField]
+    private Text[] playerFailureCounters;
+
+    // Counters.
+    private int[] orderCompletions;
+    private int[] orderFailures;
+
+    public void CompletedOrder (int player)
+    {
+        // Update order completions.
+        orderCompletions[player]++;
+        playerSuccessCounters[player].text = orderCompletions[player].ToString();
+    }
+
+    public void MissedOrder (int player)
+    {
+        // Update order failures.
+        orderFailures[player]++;
+        playerFailureCounters[player].text = orderCompletions[player].ToString();
+    }
+
+    private void Start()
+    {
+        // Size array to match number of players in inspector.
+        orderCompletions = new int[playerSuccessCounters.Length];
+        orderFailures = new int[playerSuccessCounters.Length];
+    }
+
+
+
+
+    // Testing only.
+    float lastHit = 0;
+    private void Update()
+    {
+        if (Time.time - 1 > lastHit)
+        {
+            lastHit = Time.time;
+
+            CompletedOrder(0);
+            CompletedOrder(1);
+
+            MissedOrder(1);
+            MissedOrder(0);
+        }
+    }
+}
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,19 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Timer : MonoBehaviour {
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private Text timerText;
+
+    private bool isRunning = false;
+
+    private float secondsRemaining;
 
 	// Use this for initialization.
 	void Start ()
     {
-		
+        // Testing.
+        StartTimer(120);
+
 	}
-	
+
+    // Starts the game timer.
+    public void StartTimer(float timeLimit)
+    {
+        // Set the initial timer values.
+        secondsRemaining = timeLimit;
+        timerText.text = ((int)secondsRemaining).ToString();
+        isRunning = true;
+    }
 
 	// Update is called once per frame.
 	void Update ()
     {
-		
+        if (isRunning)
+        {
+            secondsRemaining -= Time.deltaTime;
+            timerText.text = MinuteSecond((int)secondsRemaining);
+        }
 	}
+
+    // Convert seconds to minute-second format
+    private string MinuteSecond (int totalSeconds)
+    {
+        // Break total seconds into components.
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+
+        // Add seperator.
+        string returnString = minutes + ":";
+
+        // Add seconds with additional leading zero if neccasary.
+        if (seconds < 10)
+        {
+            returnString += "0" + seconds;
+        }
+        else
+        {
+            returnString += seconds;
+        }
+
+        return returnString;
+    }
 }
