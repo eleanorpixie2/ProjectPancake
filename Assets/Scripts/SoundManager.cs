66fd6fe80c831269c42e530f49a6e5de7c9fb0f3
Adrian Gilbert Ratuita <adrianratuita@gmail.com>
2018-10-12 22:08:10 -0500

    Sound Manager Skeleton

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SoundManager : MonoBehaviour
+{
+
+
+    [SerializeField]
+    CharacterControl Player1;
+    [SerializeField]
+    CharacterControl Player2;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+
+    }
+
+}
+
